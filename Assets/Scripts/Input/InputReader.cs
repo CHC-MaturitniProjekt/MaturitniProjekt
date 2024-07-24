f8ff2097f69d308bb9b55a02b9edfe7e8ba2a5f6
Tobiáš Ondra <tobiasondra2@gmal.com>
2024-07-24 22:01:19 +0200

    new input system

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(menuName = "InputReader")]
+public class InputReader : ScriptableObject, Inputs.IMainActions
+{
+    Inputs _inputs;
+
+    private void OnEnable()
+    {
+        if (_inputs == null)
+        {
+            _inputs = new Inputs();
+            _inputs.Main.SetCallbacks(this);
+            _inputs.Main.Enable();
+        }
+    }
+
+    private void OnDisable()
+    {
+        _inputs.Main.Disable();
+    }
+
+    public event Action<Vector2> MoveEvent;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        MoveEvent?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public event Action PauseEvent;
+
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        PauseEvent?.Invoke();
+    }
+
+    public event Action InteractEvent;
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.started)
+            InteractEvent?.Invoke();
+    }
+}
