6ef9e2a61fe201ea3da667cdae685089ff5fda6e
Tobias Ondra <tobiasondra2@gmail.com>
2025-03-04 11:40:06 +0100

    better interact :)

---
+++ Assets/__Scripts/Interactions/pcInteract.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pcInteract : InteractAction
+{
+    public override void OnInteract()
+    {
+        Debug.Log("PC");
+    }
+}
