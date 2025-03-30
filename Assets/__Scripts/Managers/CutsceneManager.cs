8d6994fbad557abd78be8b5805f6231565f659c9
Tesa__ <tesacze@gmail.com>
2025-03-30 12:08:07 +0200

    cutscene multi clip

--- Assets/__Scripts/Managers/CutsceneManager.cs
+++ Assets/__Scripts/Managers/CutsceneManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CutsceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Cutscene Settings")]
+    [SerializeField] private Animator[] cutsceneAnimators;
+    
+    [Header("Debug")]
+    [SerializeField] private bool startOnAwake = false;
+
+    private void Awake()
     {
-        
+        foreach (var animator in cutsceneAnimators)
+        {
+            animator.enabled = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        if (startOnAwake)
+        {
+            TriggerCutscene();
+        }
+    }
+    
+    public void TriggerCutscene()
+    {
+        StartCoroutine(PlayCutsceneSequence());
+    }
+    
+    private IEnumerator PlayCutsceneSequence()
+    {
+        for (int i = 0; i < cutsceneAnimators.Length; i++)
+        {
+            Animator animator = cutsceneAnimators[i];
+            animator.enabled = true;
+            animator.Play(animator.GetCurrentAnimatorClipInfo(0)[0].clip.name);
+
+            yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f);
+
+            animator.enabled = false;
+        }
     }
 }
