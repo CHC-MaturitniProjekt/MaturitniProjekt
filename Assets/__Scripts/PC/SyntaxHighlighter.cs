da6ed5c61319c3fa70d984dc09129a991fd4f918
Tobias Ondra <tobiasondra2@gmail.com>
2025-03-31 13:08:25 +0200

    pc

---
+++ Assets/__Scripts/PC/SyntaxHighlighter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using TMPro;
+using UnityEngine;
+
+public class SyntaxHighlighter : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    public TextMeshProUGUI syntaxHighlighter;
+
+    private void Start()
+    {
+        inputField.onValueChanged.AddListener(UpdateSyntaxHighlighting);
+    }
+
+    void UpdateSyntaxHighlighting(string text)
+    {
+        string highlightedText = text;
+
+        // Příklad pro C# klíčová slova
+        highlightedText = Regex.Replace(highlightedText, @"\b(public|private|void|class|new|if|else)\b", "<color=#ff6600>$1</color>");
+
+        // Další pravidla (čísla, stringy, komentáře)
+        highlightedText = Regex.Replace(highlightedText, @"(\"".*?\"")", "<color=#00ff00>$1</color>"); // stringy
+        highlightedText = Regex.Replace(highlightedText, @"(//.*?$)", "<color=#808080>$1</color>", RegexOptions.Multiline); // komentáře
+
+        syntaxHighlighter.text = highlightedText;
+    }
+}
