ea1af7d824cb8548ddbff0dc6d93308711aae583
Tesa__ <tesacze@gmail.com>
2025-03-31 20:49:56 +0200

    texture animation fix

--- Assets/__Scripts/TextureAnimation.cs
+++ Assets/__Scripts/TextureAnimation.cs
@@ -1,24 +1,66 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TextureAnimation : MonoBehaviour
 {
+    public enum AnimationType
+    {
+        Blinking,
+        Speaking
+    }
+
     [SerializeField] private SkinnedMeshRenderer skinnedMeshRenderer;
     [SerializeField] private float XOffsetPX;
     [SerializeField] private float YOffsetPX;
     [SerializeField] private float minBlinkTime;
     [SerializeField] private float maxBlinkTime;
+    [SerializeField] private int framesPerAnimation;
+    [SerializeField] private int numberOfAnimations;
 
     private Material faceBaseTexture;
-    
+    private Vector2 textureOffset;
+    private int currentFrame;
+    private AnimationType currentAnimation;
+
     void Start()
     {
         faceBaseTexture = skinnedMeshRenderer.material;
+        textureOffset = faceBaseTexture.mainTextureOffset;
+        
+        currentAnimation = AnimationType.Blinking;
+        
+        faceBaseTexture.mainTextureOffset = new Vector2(0, 0);
+        StartCoroutine(AnimateTexture());
     }
 
-    void Update()
+    private IEnumerator AnimateTexture()
     {
-        
+        while (true)
+        {
+            textureOffset.x = ((framesPerAnimation - 1 - currentFrame) % framesPerAnimation) * XOffsetPX;
+            textureOffset.y = (int)currentAnimation * YOffsetPX;
+            faceBaseTexture.mainTextureOffset = textureOffset;
+
+            currentFrame = (currentFrame + 1) % framesPerAnimation;
+
+            if (currentAnimation == AnimationType.Blinking && currentFrame == 1)
+            {
+                yield return new WaitForSeconds(0.2f);
+            }
+            else if (currentAnimation == AnimationType.Speaking)
+            {
+                yield return new WaitForSeconds(0.5f);
+            }
+            else
+            {
+                yield return new WaitForSeconds(Random.Range(minBlinkTime, maxBlinkTime));
+            }
+        }
+    }
+
+    public void SetAnimation(AnimationType animation)
+    {
+        currentAnimation = animation;
+        currentFrame = 0;
     }
 }
